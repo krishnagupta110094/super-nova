546d65f342f361e261c0aa9cabdd0fed939d970c
krishnagupta110094 <krishnagupta110094@gmail.com>
2026-03-21 07:00:13 +0530

    Add a Health Check Route

--- ai-buddy/src/app.js
+++ ai-buddy/src/app.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "AI Buddy Service is Running..." });
+});
 
 module.exports = app;
